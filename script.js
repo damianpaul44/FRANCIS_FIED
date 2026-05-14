7767a215900cd1507297d335d6967d461720f930
damianpaul44 <Paulagu2019@gmail.com>
2026-05-14 14:35:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -94,3 +94,40 @@ document.querySelectorAll('.faq-question').forEach(button => {
     faqItem.classList.toggle('active');
   });
 });
+
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+nextBtn.addEventListener("click", () => {
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+});
+
+prevBtn.addEventListener("click", () => {
+
+    currentSlide--;
+
+    if(currentSlide < 0){
+        currentSlide = slides.length - 1;
+    }
+
+    showSlide(currentSlide);
+});
